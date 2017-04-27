eef02bfa16e967f17d12216347ba020b57983889
Michał Urban <michalurban23@gmail.com>
2017-04-27 11:47:13 +0200

    movement is perfect

--- rambo.py
+++ rambo.py
@@ -19,7 +19,8 @@ def create_board(width, height):
     vertical = [["\bX"] * width]
     middle = []
     for i in range(height-2):
-        middle.append(list(["X"] + ["\b "] * (width-2) + ["\bX"]))
+        middle.append(list(["\bX"] + ["\b "] * (width-2) + ["\bX"]))
+    middle[2][2] = "\bX"
     playboard = vertical + middle + vertical
     return playboard
 
@@ -36,33 +37,28 @@ def insert_player(board, x, y):
 
 
 def main():
-    try:
-        width = int(sys.argv[1])
-        height = int(sys.argv[2])
-        x = int(sys.argv[3])
-        y = int(sys.argv[4])
-    except IndexError:
-        width = proper_input_size("Enter width of board: ")
-        height = proper_input_size("Enter height of board: ")
-        x = proper_input_player(width, "Enter player x position: ")
-        y = proper_input_player(height, "Enter player y position: ")
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    x = int(sys.argv[3])
+    y = int(sys.argv[4])
     while True:
         size = create_board(width, height)
         board = insert_player(size[:], x, y)
         print_board(board)
-        print("lives = 10")
+        # print("lives = 10", y, x)
+        # print("up {} down {} left {} right {}".format(board[y-2][x-1], board[y][x-1], board[y-1][x-2], board[y-1][x]))
         movement = getch()
-        if movement == "w" and y > 2:
+        # When any 'WSAD' key press and coresponding position is not "\bX" player moves
+        if movement == "w" and board[y-2][x-1] != "\bX":
             y -= 1
-        if movement == "s" and y < height-1:
+        if movement == "s" and board[y][x-1] != "\bX":
             y += 1
-        if movement == "a" and x > 2:
+        if movement == "a" and board[y-1][x-2] != "\bX":
             x -= 1
-        if movement == "d" and x < width-1:
+        if movement == "d" and board[y-1][x] != "\bX":
             x += 1
         if movement == "q":
             exit()
-        print("lives = 10")
 
 
 if __name__ == '__main__':
